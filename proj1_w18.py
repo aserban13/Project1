cd677ab19ad47ecaf27892937be50a80af9cef21
Andreea Serban <andreeaserban@Andreeas-MacBook-Pro.local>
2018-02-03 09:21:46 -0500

    Functions created

--- proj1_w18.py
+++ proj1_w18.py
@@ -7,11 +7,9 @@ import requests
 # ●	__len__: returns 0
 
 class Media:
-	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", json_dict=None):
-		if json_dict is not None:
-			self.title = jason_dict["title"]
-			self.author = json_dict["writer"]
-		else:
+	# takes title, author, and release year as arguments.
+	# Use named arguments with defaults.
+	def __init__(self, title="No Title", author="No Author", release_year="No Release Year"):
 			self.title = title
 			self.author = author
 			self.release = release_year
@@ -26,8 +24,8 @@ class Media:
 	def __len__(self):
 		return 0
 
-sample_file_name = "sample_json.txt"
-r = requests.get(sample_file_name)
+# sample_file_name = "sample_json.txt"
+# r = requests.get(sample_file_name)
 
 
 # ●	Additional instance variables: album, track length
@@ -36,31 +34,36 @@ r = requests.get(sample_file_name)
 class Song(Media):
 # ●	__init__: takes title, author, release year, album, genre, and track length as arguments.
 # Use named arguments with defaults. Call super( ) to initialize variables that belong to Media
-	def __init__(self, album="No Album", genre="None", track_len= "0"):
-		pass
+	def __init__(self, title, author, release_year, album="No Album", genre="None", track_len= "0"):
+		super().__init__(title, author, release_year)
+		self.album_name = album
+		self.genre_type = genre
+		self.track_time = track_len
 # ●	__str__: add "[<genre>]" to the end of the output from Media.__str__( ).
 # For example "Hey Jude by The Beatles (1968) [Rock]"
 	def __str__(self, genre="No Genre"):
-		pass
+		return super().__str__() + '['+ self.genre_type + ']'
 
 # ●	__len__: return track length in seconds
 	def __len__(self):
-		pass
+		return self.track_time
 
 # ●	Additional instance variables: rating, movie length
 class Movie(Media):
 # ●	__init__: takes title, author, release year, rating,
  # and movie length as arguments. Use named arguments with defaults.
  # Call super( ) to initialize variables that belong to Media.
-	def __init__(self):
-		pass
+	def __init__(self, title, author, release_year, rating="No Rating", movie_len = "0"):
+		super().__init__(title, author, relase_year)
+		self.rat = rating
+		self.ml = movie_len
 # ●	__str__: add "[<rating>]" to the end of the output from Media.__str__( ).
 # For example "Jaws by Steven Speilberg (1975) [PG]"
 	def __str__(self):
-		pass
+		return super().__str__() + '['+ self.rat + ']'
 # ●	__len__: return movie length in minutes (rounded to nearest minute)
 	def __len__(self):
-		pass
+		return round(self.ml)
 
 
 ## Other classes, functions, etc. should go here
