5280aa2f66ca3fb9d3ab687b4b7377cf884712d6
Andreea Serban <andreeaserban@0587340093.wireless.umich.net>
2018-02-05 18:16:17 -0500

    Json code added

--- proj1_w18.py
+++ proj1_w18.py
@@ -1,23 +1,55 @@
 import json
 import requests
+import math
+# import sample_json.txt
+
+def opening_json_file(url):
+	sample_file = open(url, 'r')
+	sample_data = sample_file.read()
+	sample_load = json.loads(sample_data)
+
+	for text in range(len(sample_load)):
+		if sample_load[text]["wrapperType"] == "track":
+			if sample_load[text]["kind"] == "song":
+				print("This is a song")
+				print(Song(json_dict=sample_load[text]))
+			elif sample_load[text]["kind"] == "feature-movie":
+				print("This is a movie")
+				print(Movie(json_dict=sample_load[text]))
+			else:
+				print("This is a media")
+				print(Media(json_dict=sample_load[text]))
+
+
+
+
+
+
+
+
+
 
 # Media:
 # ●	Instance variables: title, author, release year
 # ●	Methods: implement the following:
 # ●	__len__: returns 0
-
 class Media:
 	# takes title, author, and release year as arguments.
 	# Use named arguments with defaults.
-	def __init__(self, title="No Title", author="No Author", release_year="No Release Year"):
+	def __init__(self, title="No Title", author="No Author", release_year="0000", json_dict=None):
+		if json_dict is not None:
+			self.title = json_dict["collectionName"]
+			self.author = json_dict["artistName"]
+			self.release = json_dict["releaseDate"]
+			self.release = self.release[:4]
+		else:
 			self.title = title
 			self.author = author
-			self.release = release_year
+			self.release = release_year[:4]
 
 # ●	__str__: returns "<title> by <author> (<release year>)", filling in the appropriate instance variables. For example "Bridget Jones's Diary (Unabridged) by Helen Fielding (2012)."
 	def __str__(self):
-		printing = self.title + " by " + self.author
-		printing += " (" + self.release + ")\n"
+		printing = self.title + " by " + self.author + " (" + self.release + ")"
 		return printing
 
 # ●	__len__: returns 0
@@ -28,43 +60,66 @@ class Media:
 # r = requests.get(sample_file_name)
 
 
+
+
+
+
+
 # ●	Additional instance variables: album, track length
 # ●	Methods:
 
 class Song(Media):
 # ●	__init__: takes title, author, release year, album, genre, and track length as arguments.
 # Use named arguments with defaults. Call super( ) to initialize variables that belong to Media
-	def __init__(self, title, author, release_year, album="No Album", genre="None", track_len= "0"):
-		super().__init__(title, author, release_year)
-		self.album_name = album
-		self.genre_type = genre
-		self.track_time = track_len
+	def __init__(self, title="No Title", author="No Author", release_year="0000", album="No Album", genre="None", track_len='0', json_dict=None):
+		if json_dict is not None:
+			super().__init__(title, author, release_year, json_dict)
+			self.album = json_dict["collectionName"]
+			self.genre = json_dict["primaryGenreName"]
+			self.track = json_dict["trackTimeMillis"]
+		else:
+			super().__init__(title, author, release_year, None)
+			self.album = album
+			self.genre = genre
+			self.track= track_len
 # ●	__str__: add "[<genre>]" to the end of the output from Media.__str__( ).
 # For example "Hey Jude by The Beatles (1968) [Rock]"
 	def __str__(self, genre="No Genre"):
-		return super().__str__() + '['+ self.genre_type + ']'
+		return super().__str__()+' ['+ self.genre + ']'
 
 # ●	__len__: return track length in seconds
 	def __len__(self):
-		return self.track_time
+		answer = float(self.track) / 1000
+		return round(answer)
+
+
+
+
+
+
 
 # ●	Additional instance variables: rating, movie length
 class Movie(Media):
 # ●	__init__: takes title, author, release year, rating,
  # and movie length as arguments. Use named arguments with defaults.
  # Call super( ) to initialize variables that belong to Media.
-	def __init__(self, title, author, release_year, rating="No Rating", movie_len = "0"):
-		super().__init__(title, author, relase_year)
-		self.rat = rating
-		self.ml = movie_len
+	def __init__(self, title="No Title", author="No Author", release_year="0000", rating="No Rating", movie_len="0", json_dict=None):
+		if json_dict is not None:
+			super().__init__(title, author, release_year, json_dict)
+			self.rat = json_dict["contentAdvisoryRating"]
+			self.ml = json_dict["trackTimeMillis"]
+		else:
+			super().__init__(title, author, release_year, None)
+			self.rat = rating
+			self.ml = movie_len
 # ●	__str__: add "[<rating>]" to the end of the output from Media.__str__( ).
 # For example "Jaws by Steven Speilberg (1975) [PG]"
 	def __str__(self):
-		return super().__str__() + '['+ self.rat + ']'
+		return super().__str__() + ' ['+ self.rat + ']'
 # ●	__len__: return movie length in minutes (rounded to nearest minute)
 	def __len__(self):
-		return round(self.ml)
-
+		answer = float(self.ml) / 60000
+		return round(answer)
 
 ## Other classes, functions, etc. should go here
 # Main Code
--- proj1_w18_test.py
+++ proj1_w18_test.py
@@ -3,22 +3,145 @@ import proj1_w18 as proj1 #changed name to proj1
 
 class TestMedia(unittest.TestCase):
 
+# This tests the __init__ function in the Media Class
     def testConstructor(self):
         m1 = proj1.Media()
-        m2 = proj1.Media("1999", "Prince")
+        m2 = proj1.Media("1999", "Prince", "1999")
 
         self.assertEqual(m1.title, "No Title")
         self.assertEqual(m1.author, "No Author")
+        self.assertEqual(m1.release, "0000")
         self.assertEqual(m2.title, "1999")
         self.assertEqual(m2.author, "Prince")
+        self.assertEqual(m2.release, "1999")
 
+# This tests the __str__ function in the Media Class
     def test_print(self):
         m1 = proj1.Media()
-        m2 = proj2.Media()
+        m2 = proj1.Media()
         self.assertEqual(str(m1), str(m2))
 
+        m3 = proj1.Media()
+        m4 = proj1.Media("1999", "Prince", "1999")
+        self.assertFalse(str(m3) == str(m4))
+
+        self.assertEqual(str(m3), "No Title by No Author (0000)")
+        self.assertEqual(str(m4), "1999 by Prince (1999)")
+
+# This tests the __len__ function in the Media Class
     def test_length(self):
         m1 = proj1.Media()
-        m2 = proj1.Media()
+        m2 = proj1.Media("1999", "Prince")
         self.assertEqual(len(m1), 0)
+        self.assertEqual(len(m2), 0)
+
+        m3 = proj1.Media("1999", "Prince", "3:19")
+        self.assertEqual(len(m3), 0)
+# Tests out json file and if it is extracting all the data
+# and outputs the correct information
+    def test_sample_med(self):
+        url_name = "sample_json.json"
+        m1 = proj1.opening_json_file(url_name)
+
+
+
+
+
+
+
+
+
+
+class TestSong(unittest.TestCase):
+# This tests the __init__ function in the Song Class
+    def testSonginit(self):
+        s1 = proj1.Song()
+        s2 = proj1.Song("Blank Space","Taylor Swift", "2014", "1989","pop","3:50")
+
+        self.assertEqual(s1.title, "No Title")
+        self.assertEqual(s1.author, "No Author")
+        self.assertEqual(s1.release, "0000")
+        self.assertEqual(s1.album, "No Album")
+        self.assertEqual(s1.genre, "None")
+        self.assertEqual(s1.track, "0")
+
+        self.assertEqual(s2.title, "Blank Space")
+        self.assertEqual(s2.author, "Taylor Swift")
+        self.assertEqual(s2.release, "2014")
+        self.assertEqual(s2.album, "1989")
+        self.assertEqual(s2.genre, "pop")
+        self.assertEqual(s2.track, "3:50")
+# This tests the __str__ function in the Song Class
+    def testSongstr(self):
+        s1 = proj1.Song()
+        s2 = proj1.Song("Blank Space","Taylor Swift", "2014", "1989","pop","3:50")
+        s3 = proj1.Song("Welcome to New York","Taylor Swift", "2014", "1989","pop","3:50")
+        same_3 = proj1.Song("Welcome to New York","Taylor Swift", "2014", "1989","pop","3:50")
+
+        self.assertFalse(str(s1) == str(s2))
+        self.assertFalse(str(s2) == str(s3))
+        self.assertEqual(str(s3), str(same_3))
+
+        self.assertEqual(str(s2), "Blank Space by Taylor Swift (2014) [pop]")
+        self.assertEqual(str(s1), "No Title by No Author (0000) [None]")
+
+# ISSUE: Can only round to full numbers,
+# not to decimal places
+# This tests the __len__ function in the Song Class
+    def testSonglen(self):
+        s1 = proj1.Song()
+        s2 = proj1.Song("Blank Space","Taylor Swift", "2014", "1989","pop", '186600')
+
+        # print(len(s1))
+        self.assertEqual(len(s1), 0)
+        self.assertEqual(len(s2), 187)
+# Tests out json file and if it is extracting all the data
+# and outputs the correct information
+    def test_json(self):
+        pass
+
+
+
+
+
+
+
+
+
+class TestMovie(unittest.TestCase):
+
+    def testMovieinit(self):
+        m1 = proj1.Movie()
+        m2 = proj1.Movie("Jaws", "Steven Speilberg", "1975", "PG", "2.4")
+
+        self.assertEqual(m1.title, "No Title")
+        self.assertEqual(m1.author, "No Author")
+        self.assertEqual(m1.release, "0000")
+        self.assertEqual(m1.rat, "No Rating")
+        self.assertEqual(m1.ml, "0")
+
+        self.assertEqual(m2.title, "Jaws")
+        self.assertEqual(m2.author, "Steven Speilberg")
+        self.assertEqual(m2.release, "1975")
+        self.assertEqual(m2.rat, "PG")
+        self.assertEqual(m2.ml, "2.4")
+
+    def testMoviestr(self):
+        m1 = proj1.Movie()
+        m2 = proj1.Movie("Jaws", "Steven Speilberg", "1975", "PG", "7451455")
+
+        self.assertFalse(str(m1) == str(m2))
+        self.assertEqual(str(m1), "No Title by No Author (0000) [No Rating]")
+        self.assertEqual(str(m2), "Jaws by Steven Speilberg (1975) [PG]")
+
+    def testMovielen(self):
+        m1 = proj1.Movie()
+        m2 = proj1.Movie("Jaws", "Steven Speilberg", "1975", "PG", "7451455")
+
+        self.assertEqual(len(m1), 0)
+        self.assertEqual(len(m2), 124)
+# Tests out json file and if it is extracting all the data
+# and outputs the correct information
+    def test_json(self):
+        pass
 unittest.main()  #don't remove this line!
